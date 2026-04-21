f3c23bc291e8e18ab603ca426b7f848677efdb5d
Bishal Baniya <mr.bishal.baniya@gmail.com>
2026-04-21 18:25:50 +0545

    Add Header, ContactSection, Newsletter, Instagram, Footer to not-found page and fix onBackHome prop

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,28 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import NotFoundPage from '@/components/404';
+import Header from '@/components/common/header';
+import Footer from '@/components/common/Footer';
+ 
+import Newsletter from '@/components/shared/newsletter';
+import Instagram from '@/components/shared/instagram';
 
 export default function NotFound() {
-  return <NotFoundPage />;
+  const router = useRouter();
+
+  const handleBackHome = () => {
+    router.push('/');
+  };
+
+  return (
+    <>
+      <Header />
+      <NotFoundPage onBackHome={handleBackHome} />
+ 
+      <Newsletter />
+      <Instagram />
+      <Footer />
+    </>
+  );
 }
